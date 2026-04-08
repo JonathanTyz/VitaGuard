8490cb977dbd3083cc573d3bcb348709c0aece2c
agnesriby-21 <s160423099@student.ubaya.ac.id>
2026-04-08 23:03:55 +0700

    Create Migration & Seeder Batch 5

---
+++ project/database/migrations/2026_04_08_134522_prescriptions.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('doctor_username', 50);
+            $table->string('member_username', 50);
+            $table->unsignedBigInteger('appointment_id')->nullable();
+            $table->unsignedBigInteger('consultation_id')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('doctor_username')
+                ->references('username')
+                ->on('doctors')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreign('member_username')
+                ->references('username')
+                ->on('members')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreign('appointment_id')
+                ->references('id')
+                ->on('appointments')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreign('consultation_id')
+                ->references('id')
+                ->on('consultations')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
